b1df87b667e050d9b0699106389e00b452313227
marvepraise <marvepraise@gmail.com>
2022-11-28 12:36:09 -0800

    my C scrpt

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise, *hare;
+	tortoise = list;
+
+	if (!list)
+		return (0);
+	hare = list->next;
+	while (hare && hare->next)
+	{
+		if (tortoise == hare)
+			return (1);
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+	}
+	return (0);
+}
