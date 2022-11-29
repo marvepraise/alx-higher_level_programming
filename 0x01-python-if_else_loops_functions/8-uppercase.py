83ffb750c9286abefa192c127d84ca3615a76ed1
marvepraise <marvepraise@gmail.com>
2022-11-29 14:54:03 -0800

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    low = [i for i in range(97, 123)]
+    up = [j for j in range(65, 91)]
+    new_str = ""
+    for letter in str:
+        for upper, lower in zip(up, low):
+            if letter == chr(lower):
+                letter = chr(upper)
+         new_str += letter
+    print("{}".format(new_str))
