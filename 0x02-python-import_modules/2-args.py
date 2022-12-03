427b89866ec007d4937756b103cd52f2c8c12675
marvepraise <marvepraise@gmail.com>
2022-12-03 13:50:45 -0800

    fixed error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     from sys import argv
-    lenOfArg = len(argv)
+    ArgLen = len(argv)
     
-    if lenOfArg == 1:
+    if ArgLen == 1:
         print("0 arguments.")
-    elif lenOfArg == 2:
-        print("{} argument:".format(lenOfArg - 1))
+    elif ArgLen == 2:
+        print("{} argument:".format(ArgLen - 1))
     else:
-        print("{} arguments:".format(lenOfArg - 1))
+        print("{} arguments:".format(ArgLen - 1))
     for index, var in enumerate(argv):
         if index == 0:
             continue
