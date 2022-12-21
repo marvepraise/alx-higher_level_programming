a70757cef85deae259a68714690a520918b76637
marvepaise <marvepraise@gmail.com>
2022-12-20 17:21:34 -0800

    done

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+"""Square module definition.
+This module defines a simple `Square` class
+"""
+
+
+class Square:
+    """A simple ``Square`` class
+    Attributes:
+        size (`int`): The size of the ``Square``.
+    """
+
+    def __init__(self, size=0):
+        """Constructs a ``Square`` objet
+        Args:
+            size (`int`): The size of the ``Square``.
+                The default value is 0.
+        Raises:
+            TypeError: If ``size`` is not an integer.
+            ValueError: If ``size`` < 0
+        """
+        self.size = size
+
+    def area(self):
+        """Computes the area of the ``Square``.
+        Returns:
+            int: The area of the ``Square``.
+        """
+        return self._Square__size ** 2
+
+    @property
+    def size(self):
+        """
+        Args:
+            size (`int`): The size of the ``Square``.
+                The default value is 0.
+        Raises:
+            TypeError: If ``size`` is not an integer.
+                ValueError: If ``size`` < 0
+        """
+        return self._Square__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
+
+    def my_print(self):
+        """Prints a ``Square`` filled with '#'"""
+        if self.size:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print()
+        else:
+            print()
