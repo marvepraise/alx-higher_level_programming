01cf5b81beeed028a74c60d79a3fb7aa37943f89
marvepaise <marvepraise@gmail.com>
2023-01-10 15:09:56 -0800

    fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -25,4 +25,4 @@ class Student:
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict_
+        return self.__dict__
